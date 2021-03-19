f58f440dfb1082e9d428dd09544569637f64b80f
Dhruv-Rana <57038325+Dhruv-Rana@users.noreply.github.com>
2021-03-20 01:48:32 +0530

    Added Session

--- Web App/src/app.js
+++ Web App/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session') 
 const path = require('path');
 const app = express();
 const hbs = require('hbs');
@@ -6,7 +7,6 @@ const hbs = require('hbs');
 require('dotenv').config();
 require('./db/conn.js');
 const User = require('./models/users');
-const Question = require('./models/questions');
 
 const port = process.env.PORT || 3000;
 
@@ -21,6 +21,12 @@ app.use(express.static(static_path));
 app.set("view engine", "hbs");
 app.set("views", templates_path);
 hbs.registerPartials(partials_path);
+
+app.use(session({
+    secret: 'Your_Secret_Key', 
+    resave: false, 
+    saveUninitialized: false
+}));
 
 app.get('/', (req, res) => {
     res.render("index")
@@ -37,12 +43,42 @@ app.post('/login', async (req, res) => {
         
         const _user = await User.findOne({uid: uid})
         if(_user.pass === pass){
-            res.status(201).render("index");
+            res.status(201).redirect("instructions");
+            req.session.id = uid;
         } else{
             res.send("Invalid Credentials!")
         }
     } catch (e) {
         res.status(400).send("Invalid Credentials!");
+    }
+});
+
+app.get('/instructions', (req, res) => {
+    res.render("instructions");
+});
+
+app.post('/instructions', async (req, res) => {
+    try{
+        res.status(201).redirect("test");
+    } catch (e) {
+        res.status(400).send("OOPS Something went wrong!");
+    }
+});
+
+app.get('/test', (req, res) => {
+    res.render("test");
+    try{
+
+    } catch(e){
+
+    }
+});
+
+app.post('/test', async (req, res) => {
+    try{
+        //res.status(201).redirect("test");
+    } catch (e) {
+        res.status(400).send("OOPS Something went wrong!");
     }
 });
 
